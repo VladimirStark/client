6816ae094b9d98f519a064c78e17dabf2f04df3c
Zaur <24zaur@rambler.ru>
2023-10-10 22:00:59 +0300

    first

---
+++ src/todolist/Todolist.jsx
@@ -0,0 +1,36 @@
+import { Container, Divider, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import TasksList from './TasksList'
+
+const initialTasks = [
+    {
+      id: 1,
+      title: 'Learn JS',
+      done: true
+    },
+    {
+      id: 2,
+      title: 'Learn React',
+      done: false
+    }
+  ]
+
+export default function Todolist() {
+    const [tasks, setTasks] = useState(initialTasks)
+  return (
+    <Container maxWidth='xl'>
+      <Typography 
+      variant='h4' 
+      component='h1'
+        sx={{
+          my: 4,
+          textAlign: 'center'
+        }}
+      >
+          Personal task manager
+      </Typography>
+      <Divider></Divider> 
+      <TasksList></TasksList>     
+    </Container>
+  )
+}
